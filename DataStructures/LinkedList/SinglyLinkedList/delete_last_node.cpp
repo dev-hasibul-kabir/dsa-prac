d235bc9d75ce5542d13dc714d068dc1651162e3e
Hasibul Kabir <hasibul.kabir.dev@gmail.com>
2025-07-12 11:44:54 +0600

    Delete last value

---
+++ DataStructures/LinkedList/SinglyLinkedList/delete_last_node.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+public:
+    int value;
+    Node *next;
+
+    Node(int value){
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int el){
+    Node *new_node = new Node(el);
+
+    if(head == NULL){
+        head = new_node;
+        return;
+    }
+    Node *temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new_node;
+
+}
+
+void delete_last(Node *&head){
+    if(head == NULL)return;
+    if(head->next == NULL){
+        delete head;
+        head = NULL;
+        return;
+    }
+
+      Node *temp = head;
+
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    Node *last = temp->next;
+    temp->next = NULL;
+    delete last;
+
+}
+
+
+void print_list(Node *head){
+    cout << "Linked List-" << endl;
+    while(head != NULL){
+        cout << head->value << endl;
+        head = head->next;
+    }
+
+}
+
+int main(){
+    Node *head = NULL;
+    int n;
+    int el;
+
+    cout << "Enter the list size: ";
+    cin >> n;
+
+    // Insert at the last position
+    for(int i = 0; i < n; i++){
+        cout << "Enter value " << i+1 << ": ";
+        cin >> el;
+
+        insert_at_tail(head, el);
+    }
+    print_list(head);
+
+    //Delete last value
+     delete_last(head);
+
+     //Print again to check the list
+    print_list(head);
+
+
+    return 0;
+}
+
