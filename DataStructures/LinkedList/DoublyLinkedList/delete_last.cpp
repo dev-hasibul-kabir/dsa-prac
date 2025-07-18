04eade8667598c981aeeb6ee55d4e3ffe3bff1e1
Hasibul Kabir <hasibul.kabir.dev@gmail.com>
2025-07-18 12:31:13 +0600

    removed redundant code

--- DataStructures/LinkedList/DoublyLinkedList/delete_last.cpp
+++ DataStructures/LinkedList/DoublyLinkedList/delete_last.cpp
@@ -54,19 +54,7 @@ void print(Node *head){
 int main(){
     Node *head = NULL;
     Node *tail = NULL;
-    /*Node *head = new Node(10);
-    Node *a = new Node(20);
-    Node *b = new Node(30);
-    Node *c = new Node(40);
-
-    head->next = a;
-    a->prev = head;
-    a->next = b;
-    b->prev = a;
-    b->next = c;
-    c->prev = b;
-
-    Node *tail = c;*/
+  
 
     int val;
     while(true){
