5b34ea20918ed5477b771f0840125bdeacad8101
Hasibul Kabir <hasibul.kabir.dev@gmail.com>
2025-08-10 17:06:11 +0600

    Time: 0 ms (100%), Space: 9.9 MB (5.89%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+
+        string word, lastWord;
+        while(ss >> word){
+            lastWord = word;
+        }
+
+        return lastWord.size();
+    }
+};
