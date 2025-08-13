feca2c626bb94d31008802d8e1cb6fbea3720f5c
Hasibul Kabir <hasibul.kabir.dev@gmail.com>
2025-08-13 08:34:42 +0600

    Is given value present in the binary tree or not

---
+++ DataStructures/BinaryTree/BinaryTreeProblemSolving/isValuePresent.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+bool isNodePresent(Node* root, int x){
+    if(root == NULL) return false;
+    if(root->val == x) return true;
+
+    bool l = isNodePresent(root->left, x);
+    bool r = isNodePresent(root->right, x);
+    return l || r;
+}
+
+int main(){
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+    Node *j = new Node(110);
+    Node *k = new Node(120);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    d->left = g;
+    d->right = h;
+    b->left = e;
+    b->right = f;
+    e->right = i;
+    f->left = j;
+    f->right = k;
+
+    cout << isNodePresent(root, 50);
+
+    return 0;
+}
