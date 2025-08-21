3c5d779f16df770d533e40b6fe7ea335947f19f1
Hasibul Kabir <hasibul.kabir.dev@gmail.com>
2025-08-21 11:47:10 +0600

    max heap

---
+++ DataStructures/Heap/MaxHeap/InsertInMaxHeap/insert_in_max_heap.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> tree = {10, 5, 3, 2, 4};
+   int x;
+   cout << "Enter value to insert: ";
+   cin >> x;
+   tree.push_back(x);
+
+   int curr_index = tree.size() - 1;
+
+   while(curr_index != 0){
+        int parent_index = (curr_index -1)/2;
+    if(tree[parent_index] < tree[curr_index]){
+        swap(tree[parent_index], tree[curr_index]);
+        curr_index = parent_index;
+    }else{
+        break;
+    }
+   }
+
+   for(int i : tree){
+    cout << i << " ";
+   }
+
+
+    return 0;
+}
